b0cce16e82ed52040d9d3409574800fcabc18c1c
AmanPatne <apatne78@gmail.com>
2023-11-23 00:30:33 +0530

    Add a static HTML page to display songs

--- tests/Feature/SongsTest.php
+++ tests/Feature/SongsTest.php
@@ -1,30 +1,23 @@
 <?php
-
 namespace Tests\Feature;
-
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
-
 class SongsTest extends TestCase
-{
-    /**
+{/**
      * A basic feature test example.
      */
     public function test_example(): void
-    {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
-    }
-    /**
-     * A basic feature test example.
-     */
-    public function testSongsOk(): void
     {
         $response = $this->get('/songs');
 
         $response->assertStatus(200);
     }
+public function songs_static() {
+        $httpClient = new \GuzzleHttp\Client();
+        $response = $httpClient->get('/songs_static');
 
+        // Failing assertion to expect a 200 OK status code
+        $this->assertEquals(200, $response->getStatusCode());
+    }
 }
