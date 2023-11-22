476fe3cfb7616c0f215bca213d32bd936686838a
AmanPatne <apatne78@gmail.com>
2023-11-22 09:03:10 +0530

    Render songs from Song objects

---
+++ resources/views/songs.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<table>
+<tr>
+<th>Title</th>
+<th>Artist</th>
+</tr>
+@foreach ($songs as $song)
+<tr>
+<td>{{ $song->getTitle() }}</td>
+<td>{{ $song->getArtist() }}</td>
+</tr>
+@endforeach
+</table>
+</body>
+</html>
+
+
--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,28 @@ Route::get('/', function () {
 Route::get('/veggies/{veggieName}', function (string $veggiename) {
         return $veggiename;
 })->whereIn('veggieName', ['baigan', 'bhindi', 'aaloo', 'gobhi']);
+
+use Practicals\Song;
+/*Route::get('/songs', function () {
+    return view('songs');
+});
+Route::get('/songs', function () {
+    $song = new Song();
+    $song->setTitle('With You');
+    return view('songs', [ 'song' => $song ]);
+});*/
+Route::get('/songs', function () {
+  $song1 = new Song();
+  $song1->setTitle("Stan");
+  $song1->setArtist("Eminem");
+
+  $song2 = new Song();
+  $song2->setTitle("Nothing Else Matters");
+  $song2->setArtist("Metallica");
+
+  $song3 = new Song();
+  $song3->setTitle("With You");
+  $song3->setArtist("A P Dhillon");
+
+  return view('songs', [ 'songs' => [ $song1, $song2, $song3 ] ]);
+});
